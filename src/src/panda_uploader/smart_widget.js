0f2d1e4d28cb2313029beb1ea1406259ed5c82bb
Vivien Schilis <vivien.schilis@gmail.com>
2012-04-13 15:25:39 +0100

    panda uploader, cdn only

--- src/src/panda_uploader/smart_widget.js
+++
@@ -1,9 +0,0 @@
-
-PandaUploader.SmartWidget = function(html5_opts, flash_opts) {
-    if (PandaUploader.supportHTML5Widget()) {
-        return new PandaUploader.HTML5Widget(html5_opts);
-    }
-    else {
-        return new PandaUploader.FlashWidget(flash_opts);
-    }
-};
--- src/test/app/app.rb
+++
@@ -1,5 +0,0 @@
-APP_ROOT = File.dirname(File.expand_path(__FILE__))
-$: << File.join(APP_ROOT, 'lib')
-
-require 'rubygems'
-require 'panda_uploader_test_app'
